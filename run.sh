a7833f2454f817b18ce1daea2c4790513661d360
framere <francesco.mereto@rwth-aachen.de>
2026-05-11 17:14:26 +0200

    run file

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH -J run_test
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p andoria
+#SBATCH --time=3-00:00:00
+
+JULIA=/home/fmereto/.julia/juliaup/julia-1.12.6+0.x64.linux.gnu/bin/julia
+
+$JULIA He.jl
