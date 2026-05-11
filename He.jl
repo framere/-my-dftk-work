19e35b9a11b197209d6577f29eab019abd45c9f2
framere <francesco.mereto@rwth-aachen.de>
2026-05-11 16:50:09 +0200

    Update He.jl

--- He.jl
+++ He.jl
@@ -89,17 +89,18 @@ function main()
     
     # run LOBPCG for DSV's
     # this solves the equation Kk_virt * f = ham_hf_levelshifted * λ * f 
-    # @time dsv = DFTK.LOBPCG(
-    #     Kk_virt, 
-    #     ϕk, 
-    #     ham_hf_levelshifted, 
-    #     kinetic_preconditioner, 
-    #     1e-5, 
-    #     500, 
-    #     callback=DFTK.DefaultLobpcgCallback()
-    # )  
-    println("Run Davidson for DSVs")
-    @time Σ_dsv, X_dsv = davidson(Kk_virt, ϕk, ψocck, N*8, 1e-5)
+    @time dsv = DFTK.LOBPCG(
+        Kk_virt, 
+        ϕk, 
+        ham_hf_levelshifted, 
+        kinetic_preconditioner, 
+        1e-5, 
+        500, 
+        callback=DFTK.DefaultLobpcgCallback()
+    )  
+    X_dsv = dsv.X
+    # println("Run Davidson for DSVs")
+    # @time Σ_dsv, X_dsv = davidson(Kk_virt, ϕk, ψocck, N*8, 1e-5)
 
     # we finally re-canonicalize the virtual DSV orbitals
     println("Recanonicalize DSVs.")
